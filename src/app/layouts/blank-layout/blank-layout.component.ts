ce6b63178150ca8c81c87b9e80be8f99e03bebe7
bcabanes <contact@benjamincabanes.com>
2017-08-26 19:53:00 -0400

    feat(layouts): add multiple layouts ⧓

---
+++ src/app/layouts/blank-layout/blank-layout.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-blank-layout',
+  templateUrl: './blank-layout.component.html'
+})
+export class BlankLayoutComponent {
+}
---
+++ src/app/layouts/index.ts
@@ -0,0 +1,10 @@
+import { BlankLayoutComponent } from './blank-layout';
+import { CommonLayoutComponent } from './common-layout';
+
+export const LAYOUTS_COMPONENTS: any[] = [
+  BlankLayoutComponent,
+  CommonLayoutComponent
+];
+
+export * from './blank-layout';
+export * from './common-layout';
