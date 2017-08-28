c96fdfc8d7e3eaed1dc0b9c8eafd2a9e8e807663
bcabanes <contact@benjamincabanes.com>
2017-08-27 21:48:49 -0400

    feat(user): User is now a class

--- src/app/user/ngrx/user.state.ts
+++ src/app/user/ngrx/user.state.ts
@@ -1,22 +1,56 @@
-export interface User {
+export class IUser {
   displayName: string | null;
   email: string | null;
   emailVerified: boolean;
   phoneNumber: string | null;
   photoURL: string | null;
-  providerId: string;
-  uid: string;
+  providerId: string | null;
+  uid: string | null;
   isAnonymous: boolean;
+}
+
+export class User implements IUser {
+  displayName = null;
+  email = null;
+  emailVerified = false;
+  phoneNumber = null;
+  photoURL = null;
+  providerId = null;
+  uid = null;
+  isAnonymous = false;
+
+  constructor(object: any) {
+    for (const property in this) {
+      if (this.hasOwnProperty(property) && object.hasOwnProperty(property)) {
+        this[ property ] = object[ property ];
+      }
+    }
+
+    console.log(this);
+  }
+
+  serialize(): IUser {
+    const serializedUser = {};
+    for (const property in this) {
+      if (this.hasOwnProperty(property)) {
+        Object.defineProperty(serializedUser, property, {
+          enumerable: true,
+          value     : this[ property ]
+        });
+      }
+    }
+    return serializedUser as IUser;
+  }
 }
 
 export namespace UserState {
   export interface IState {
-    current?: User;
+    current?: IUser;
     errors?: Array<any>;
   }
 
   export const initialState: IState = {
     current: null,
-    errors: []
+    errors : []
   };
 }
