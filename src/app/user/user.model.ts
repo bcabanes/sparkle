24b00ff6f3a8a41d8e4892ff93330802f703533a
bcabanes <contact@benjamincabanes.com>
2017-08-30 21:02:48 -0400

    feat(user): separate user state & user model

---
+++ src/app/user/user.model.ts
@@ -0,0 +1,44 @@
+export class IUser {
+  displayName: string | null;
+  email: string | null;
+  emailVerified: boolean;
+  phoneNumber: string | null;
+  photoURL: string | null;
+  providerId: string | null;
+  uid: string | null;
+  isAnonymous: boolean;
+}
+
+export class User implements IUser {
+  displayName = null;
+  email = null;
+  emailVerified = false;
+  phoneNumber = null;
+  photoURL = null;
+  providerId = null;
+  uid = null;
+  isAnonymous = false;
+
+  constructor(object: any) {
+    for (const property in this) {
+      if (this.hasOwnProperty(property) && object.hasOwnProperty(property)) {
+        this[ property ] = object[ property ];
+      }
+    }
+
+    console.log(this);
+  }
+
+  serialize(): IUser {
+    const serializedUser = {};
+    for (const property in this) {
+      if (this.hasOwnProperty(property)) {
+        Object.defineProperty(serializedUser, property, {
+          enumerable: true,
+          value     : this[ property ]
+        });
+      }
+    }
+    return serializedUser as IUser;
+  }
+}
