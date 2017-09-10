d06179b4285b326a165173236962cc81e06c92e3
bcabanes <contact@benjamincabanes.com>
2017-09-10 14:34:24 -0400

    feat(dashboard): clean component

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -15,25 +15,12 @@ import { CardActions } from '../cards/ngrx/card.action';
 })
 export class DashboardComponent implements OnInit {
   deckList$: Observable<IDeck[]>;
-  cardList$: Observable<ICard[]>;
 
   constructor(private store: Store<IAppState>) {}
 
   ngOnInit() {
     this.store.dispatch(new DeckActions.LoadDeckListAction());
-    this.store.dispatch(new CardActions.LoadCardListAction('-Kt-NpzonfXTwtMDogim'));
-    this.cardList$ = this.store.select(s => s.card.list);
     this.deckList$ = this.store.select(s => s.deck.list);
-  }
-
-  public createCard() {
-    const card = new Card({
-      title: 'CardTitle',
-      content: `Card content ${(Math.random() * 100).toFixed()}`,
-      type: 'card',
-      deckUid: '-Kt-NpzonfXTwtMDogim'
-    });
-    this.store.dispatch(new CardActions.CreateCardAction(card.serialize()));
   }
 
   public createDeck() {
@@ -43,10 +30,6 @@ export class DashboardComponent implements OnInit {
       type: 'card'
     });
     this.store.dispatch(new DeckActions.CreateDeckAction(deck.serialize()));
-  }
-
-  public deleteCard(cardUid) {
-    this.store.dispatch(new CardActions.DeleteCardAction(cardUid));
   }
 
   public deleteDeck(deckUid) {
