cfde3557f9f88300cdd6bbe1569f80a59c12e951
bcabanes <contact@benjamincabanes.com>
2017-09-10 14:33:56 -0400

    feat(deck): improve deck component (delete)

--- src/app/decks/components/deck-list.component.ts
+++ src/app/decks/components/deck-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { IDeck } from '../deck.model';
 
 @Component({
@@ -10,10 +10,13 @@ import { IDeck } from '../deck.model';
         <md-icon md-list-icon>note</md-icon>
         <h4 md-line>{{deck.title}}</h4>
         <a md-button [routerLink]="[ '/manage', 'decks', deck.uid ]">manage</a>
+        <button (click)="onDelete.emit(deck.uid)" md-button>delete</button>
       </md-list-item>
     </md-list>
   `
 })
 export class DeckListComponent {
   @Input() deckList: IDeck[] = [];
+
+  @Output() onDelete: EventEmitter<string> = new EventEmitter();
 }
