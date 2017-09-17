930b9bbd00874407e8eb1d5e3341a408e7121dc5
bcabanes <contact@benjamincabanes.com>
2017-09-17 19:28:03 -0400

    feat(deck): add getAnswerableDeckList method

--- src/app/decks/deck.service.ts
+++ src/app/decks/deck.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { AngularFireAuth } from 'angularfire2/auth';
 import { AngularFireDatabase } from 'angularfire2/database';
 import { Observable } from 'rxjs/Observable';
+import 'rxjs/operator/first';
+import 'rxjs/operator/map';
 // app
 import { IDeck } from './deck.model';
 import { CardService } from '../cards/card.service';
@@ -45,5 +47,34 @@ export class DeckService {
         const path = `${this.afAuth.auth.currentUser.uid}/${this.deckPath}`;
         return Observable.fromPromise(this.db.list(path).remove(deckUid));
       });
+  }
+
+  public getAnswerableDeckList(): Observable<IDeck[]> {
+    const path = `${this.afAuth.auth.currentUser.uid}/${this.deckPath}`;
+    return this.db.list(path, {
+      query: {
+        orderByChild: 'isActive',
+        equalTo     : true
+      }
+    })
+      .map((deckList: IDeck[]) => deckList.filter(this.isDeckAnswerable))
+      .first();
+  }
+
+  private isDeckAnswerable(deck: IDeck): boolean {
+    const hoursIndicator: number = new Date().getHours();
+    if (deck.startTime < hoursIndicator && hoursIndicator < deck.endTime) {
+      const now: number = Date.now();
+      const timeSinceLastAnswer: number = ((now - deck.lastAnswered) / (1000 * 60 * 60)) % 24;
+      if (timeSinceLastAnswer > deck.ratio) {
+        console.log('should ask', timeSinceLastAnswer, deck.ratio);
+        return true;
+      } else {
+        console.log('should not ask, ratio exceeded', timeSinceLastAnswer, deck);
+      }
+    } else {
+      console.log('should not ask, interval exceeded', deck);
+    }
+    return false;
   }
 }
