b6a5f1032d91ebb9b396adc4ad5927c7c456f8af
bcabanes <contact@benjamincabanes.com>
2017-10-07 17:32:02 -0400

    feat(dashboard): add activatedDeckList count

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -13,12 +13,14 @@ import { DeckActions } from '../decks/ngrx/deck.action';
 })
 export class DashboardComponent implements OnInit {
   deckList$: Observable<IDeck[]>;
+  activatedDeckList$: Observable<IDeck[]>;
 
   constructor(private store: Store<IAppState>) {}
 
   ngOnInit() {
     this.store.dispatch(new DeckActions.LoadDeckListAction());
     this.deckList$ = this.store.select(s => s.deck.list);
+    this.activatedDeckList$ = this.deckList$.map(deckList => deckList.filter(deck => deck.isActive));
   }
 
   public deleteDeck(deckUid) {
